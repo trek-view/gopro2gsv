e8b1034a57bba5127746566b8b0911241cfbf9be
Chaos Pjeles <chaos@fqr.us>
2023-09-02 11:20:14 +0100

    fixed up newpath generation, fixes #4

--- src/__main__.py
+++ src/__main__.py
@@ -135,14 +135,16 @@ def main(args, is_photo_mode):
             make_video_gsv_compatible(mp4_file, gpx_file, final_mp4_file, is_gpmd=args.video_telemetry_format == "GPMD")
             logger.info(f"copying metadata from first image into {final_mp4_file}")
             copy_metadata_from_file(first_image['newpath'], final_mp4_file)
-            videos.append((final_mp4_file, int(first_image["File:ImageWidth"]), int(first_image["File:ImageHeight"]), images[start:end]))
+            videos.append((final_mp4_file, int(first_image["File:ImageWidth"]), int(first_image["File:ImageHeight"]), images[start:end], final_mp4_file))
     else:
         input_vid : Path = args.input_video
         if not args.output_filepath:
             name, _ = os.path.splitext(input_vid.name)
-            args.output_filepath = input_vid.with_name(f"{name}-gopro2gsv_output")
-        output_filename, _ = os.path.splitext(args.output_filepath.name)
-        log_filepath    = args.output_filepath.with_name(output_filename+".log")
+            output_filepath = input_vid.with_name(f"{name}-gopro2gsv_output")
+        else:
+            output_filepath = Path(args.output_filepath)
+        output_filename, _ = os.path.splitext(output_filepath.name)
+        log_filepath    = output_filepath.with_name(output_filename+".log")
         streams = get_streams(input_vid)
         if len(list(filter(lambda x: x.type == "video", streams))) > 1:
             raise FatalException("More than one video stream in input_video")
@@ -155,19 +157,21 @@ def main(args, is_photo_mode):
             raise FatalException(f"Less than 10 valid frames in video: {len(v)} found")
         width, height = int(metadata['Track1:ImageWidth']), int(metadata['Track1:ImageHeight'])
         logger.info(f"Found {width}x{height} video with {valid_frames} valid frames at {input_vid}")
-        videos.append((input_vid, width, height, metadata))
+        videos.append((input_vid, width, height, metadata, output_filepath))
     
-    for i, (video, width, height, more) in enumerate(videos):
-        name, _ = os.path.splitext(video.name)
-        newpath = video
+    for i, (video, width, height, more, output_path) in enumerate(videos):
+        name, _ = os.path.splitext(output_path.name)
+        newpath: Path = video
         logger.info(f"Post-processing video #{i+1} of {len(videos)}")
         if args.path_to_nadir:
             logger.info("Adding nadir to video")
-            newpath = video.with_name(f"{name}_with-nadir.mp4")
+            newpath = output_path.with_name(f"{name}_with-nadir.mp4")
+            newpath.parent.mkdir(exist_ok=True, parents=True)
             overlay_nadir(video, args.path_to_nadir, newpath, width, height)
         elif args.watermark:
             logger.info("Adding watermark to video")
-            newpath = video.with_name(f"{name}_watermarked.mp4")
+            newpath = output_path.with_name(f"{name}_watermarked.mp4")
+            newpath.parent.mkdir(exist_ok=True, parents=True)
             overlay_nadir(video, args.watermark, newpath, width, height, True)
         if args.upload_to_streetview:
             logger.info(f"Uploading `{newpath}` to GSV")
@@ -178,6 +182,8 @@ def main(args, is_photo_mode):
             database.insert_output(args.input_directory.absolute(), newpath, log_filepath, streetview_info=streetview_id, is_photo_mode=True, images=more)
         else:
             database.insert_output(args.input_video.absolute(), newpath, log_filepath, streetview_info=streetview_id, is_photo_mode=False)
+
+        logger.info(f"Video #{i+1} saved at `{newpath}`")
 
 if __name__ == "__main__":
     load_dotenv()
