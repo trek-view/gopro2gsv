3dc96a693e12f49d84ff6c54a95005d4e2e40d93
Chaos Monxrch <chaos@efqr.dev>
2023-12-02 05:08:36 +0100

    add m1 option in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 import platform
 import shutil
 from pathlib import Path
@@ -13,17 +13,26 @@ BIN_DEST = Path(os.path.abspath("src/bin"))
 
 class BuildCommand(build_py):
     def run(self):
-        binary_files: list[Path] = []
+        binary_files: list[Path] = [Path("third-party/max2sphere/max2sphere"), Path("third-party/fusion2sphere/fusion2sphere")]
         if platform.system() == "Windows":
+            binary_files.clear()
             os.system("cd .\\third-party\\fusion2sphere && make clean && make")
             os.system("cd .\\third-party\\max2sphere && make clean && make")
             binary_files.append(Path("third-party/max2sphere/max2sphere.exe"))
             binary_files.append(Path("third-party/fusion2sphere/fusion2sphere.exe"))
+        elif sys.platform.lower() == "darwin":
+            if platform.processor() == "arm": 
+                #it's Apple Silicon Mac (M1,, M2, ...)
+                os.system("cd ./third-party/fusion2sphere && make clean && make -f Makefile-MacM1")
+                os.system("cd ./third-party/max2sphere && make clean && make -f Makefile-MacM1")
+            else:
+                # regular intel mac 
+                os.system("cd ./third-party/fusion2sphere && make clean && make")
+                os.system("cd ./third-party/max2sphere && make clean && make")
         else:
+            # linux
             os.system("cd ./third-party/fusion2sphere && make clean && make")
-            os.system("cd ./third-party/max2sphere && make clean && make")
-            binary_files.append(Path("third-party/max2sphere/max2sphere"))
-            binary_files.append(Path("third-party/fusion2sphere/fusion2sphere"))
+            os.system("cd ./third-party/max2sphere && make clean && make -f Makefile-Linux")
         BIN_DEST.mkdir(exist_ok=True)
         for bin in binary_files:
             shutil.move(bin, BIN_DEST/bin.name)
@@ -31,7 +40,7 @@ class BuildCommand(build_py):
 
 setup(
     name='gopro2gsv',
-    version='0.0.1',
+    version='0.0.1-1',
     description='Does stuff with gopro files',
     cmdclass={
         'build_py': BuildCommand,
